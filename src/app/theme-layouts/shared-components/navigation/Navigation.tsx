bcef3ba2e8a29c5bec530763fad6fc08f2ed4f91
Ankit <ankit@softradix.com>
2024-02-12 19:49:42 +0530

    sidebar design done

--- src/app/theme-layouts/shared-components/navigation/Navigation.tsx
+++ src/app/theme-layouts/shared-components/navigation/Navigation.tsx
@@ -1,13 +1,13 @@
-import FuseNavigation from '@fuse/core/FuseNavigation';
-import clsx from 'clsx';
-import { useMemo } from 'react';
-import { useAppDispatch } from 'app/store/store';
-import useThemeMediaQuery from '@fuse/hooks/useThemeMediaQuery';
-import { FuseNavigationProps } from '@fuse/core/FuseNavigation/FuseNavigation';
-import { useSelector } from 'react-redux';
-import withSlices from 'app/store/withSlices';
-import { navigationSlice, selectNavigation } from './store/navigationSlice';
-import { navbarCloseMobile } from '../navbar/store/navbarSlice';
+import FuseNavigation from "@fuse/core/FuseNavigation";
+import clsx from "clsx";
+import { useMemo } from "react";
+import { useAppDispatch } from "app/store/store";
+import useThemeMediaQuery from "@fuse/hooks/useThemeMediaQuery";
+import { FuseNavigationProps } from "@fuse/core/FuseNavigation/FuseNavigation";
+import { useSelector } from "react-redux";
+import withSlices from "app/store/withSlices";
+import { navigationSlice, selectNavigation } from "./store/navigationSlice";
+import { navbarCloseMobile } from "../navbar/store/navbarSlice";
 
 /**
  * Navigation
@@ -16,33 +16,33 @@ import { navbarCloseMobile } from '../navbar/store/navbarSlice';
 type NavigationProps = Partial<FuseNavigationProps>;
 
 function Navigation(props: NavigationProps) {
-	const { className = '', layout = 'vertical', dense, active } = props;
+  const { className = "", layout = "vertical", dense, active } = props;
 
-	const navigation = useSelector(selectNavigation);
+  const navigation = useSelector(selectNavigation);
 
-	const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down('lg'));
+  const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down("lg"));
 
-	const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-	return useMemo(() => {
-		function handleItemClick() {
-			if (isMobile) {
-				dispatch(navbarCloseMobile());
-			}
-		}
+  return useMemo(() => {
+    function handleItemClick() {
+      if (isMobile) {
+        dispatch(navbarCloseMobile());
+      }
+    }
 
-		return (
-			<FuseNavigation
-				className={clsx('navigation flex-1', className)}
-				navigation={navigation}
-				layout={layout}
-				dense={dense}
-				active={active}
-				onItemClick={handleItemClick}
-				checkPermission
-			/>
-		);
-	}, [dispatch, isMobile, navigation, active, className, dense, layout]);
+    return (
+      <FuseNavigation
+        className={clsx("navigation flex-1", className)}
+        navigation={navigation}
+        layout={layout}
+        dense={dense}
+        active={active}
+        onItemClick={handleItemClick}
+        checkPermission
+      />
+    );
+  }, [dispatch, isMobile, navigation, active, className, dense, layout]);
 }
 
 export default withSlices<NavigationProps>([navigationSlice])(Navigation);
