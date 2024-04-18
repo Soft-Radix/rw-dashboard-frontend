77e96b38f957b0096d0ef2e87fff9856438e65b5
nasrudeen <nasrudeen@softradix.in>
2024-04-18 12:24:32 +0530

    profile pages design

--- src/app/components/client/ClientDetail.tsx
+++ src/app/components/client/ClientDetail.tsx
@@ -1,10 +1,11 @@
-import { Button, Box, Grid, Checkbox, Typography, TableCell, Tab, Tabs, TableRow, Theme } from "@mui/material";
+import { Button, Box, Grid, Checkbox, Typography, TableCell, Tab, Tabs, TableRow, Theme, IconButton } from "@mui/material";
 import { useTheme } from "@mui/styles";
 import { useFormik } from "formik";
 import {
   ArrowRightCircleIcon,
   DeleteIcon,
   EditIcon,
+  LastPayment,
 } from "public/assets/icons/common";
 import { PlusIcon } from "public/assets/icons/dashboardIcons";
 import { useState } from "react";
@@ -17,6 +18,7 @@ import CommonPagination from "src/app/components/pagination";
 import AddAgentModel from "src/app/components/agents/AddAgentModel";
 import { CalenderIcon, CalenderIconActive, KanbanIcon, KanbanIconActive, TaskListIcon, TaskListIconActive, TaskTableIcon, TaskTableIconActive } from "public/assets/icons/projectsIcon";
 import { AttachmentIcon } from "public/assets/icons/supportIcons";
+import EditProfile from "../profile/EditProfile";
 
 const rows = [
   {
@@ -199,19 +201,25 @@ export default function ClientDetail() {
               </Tabs>
             </>
             <CustomTabPanel value={value} index={0}>
-              <Grid container className="p-0">
-                <Grid item xs={12} sm={12} md={12} className="p-0">
-                  <div className="flex flex-col gap-10 bg-[#FFFFFF] h-auto md:h-[calc(100vh-164px)] sm:h-auto  rounded-12 ">
-                    <div className="border border-[#E7E8E9] rounded-lg flex  justify-left gap-[30px] items-start p-[2rem] flex-col sm:flex-row">
+              <Grid container className="h-auto p-0 mb-[30px]">
+                <Grid item xs={12} sm={12} md={12} className="p-0 ">
+                  <div className="flex flex-col  gap-10 bg-[#FFFFFF] h-auto rounded-12 ">
+                    <div className="border border-[#E7E8E9] rounded-lg flex  justify-left gap-[30px] items-start p-[2rem] flex-col sm:flex-row relative ">
                       <div className="h-[100px] w-[100px] sm:h-[100px] sm:w-[126px] rounded-full overflow-hidden">
                         <img
-                          src="/assets/images/avatars/male-01.jpg"
+                          src="/assets/images/avatars/male-21.jpg"
                           alt=""
                           className="h-[100px] w-[100px] rounded-full"
                         />
                       </div>
                       <div className="pt-20">
+                        <div className="flex rounded-full py-[1rem] px-[2rem] text-secondary bg-secondary_bg w-max gap-[20px] text-lg font-600 items-center absolute right-[2rem] top-[2rem]">
+                          Edit
+                          <EditIcon fill="#4F46E5" />
+                        </div>
+
                         <div className="flex items-center gap-40 mb-10">
+
                           <span className="text-[24px] text-[#111827] font-semibold inline-block">
                             Alexandra
                           </span>
@@ -266,27 +274,69 @@ export default function ClientDetail() {
                   </div>
                 </Grid>
 
-                <Grid item lg={12} className="basis-full">
-
-                  <div className="flex items-center justify-between gap-10 p-24 rounded-lg shadow-md bg-secondary_bg">
-                    <div>
-                      <Typography
-                        component="h4"
-                        className="mb-8 text-2xl text-title font-600"
-                      >
-                        Change Password
-                      </Typography>
-                      <p className="text-para_light">
-                        <div><img src="../assets/icons/lock.svg" /></div> For security purposes, if you wish to change your password, please click here to change.
-                      </p>
+                <Grid item lg={12} className="basis-full mt-[30px]">
+                  <div className="flex items-center justify-between gap-10 p-24 rounded-lg bg-secondary_bg">
+                    <div className="flex gap-[20px] items-end justify-center">
+                      <div className="bg-secondary h-[54px] w-[54px] min-w-[54px] rounded-8 flex items-center justify-center"><img src="../assets/icons/lock.svg" /></div>
+                      <div>
+                        <Typography
+                          component="h4"
+                          className="mb-8 text-2xl text-title font-600"
+                        >
+                          Change Password
+                        </Typography>
+                        <p className="text-para_light">
+                          For security purposes, if you wish to change your password, please click here to change.
+                        </p>
+                      </div>
                     </div>
                     <div className="shrink-0 w-[5rem] aspect-square flex items-center justify-center border rounded-lg border-borderColor">
                       <ArrowRightCircleIcon />
                     </div>
                   </div>
-
                 </Grid>
-              </Grid >
+              </Grid>
+
+              <Grid container spacing="26px" className="">
+                <Grid item lg={6} className="basis-full">
+                  <Link to="/change-password" className="contents">
+                    <div className="flex items-center justify-between gap-10 p-24 rounded-lg bg-bgGrey">
+                      <div>
+                        <Typography
+                          component="h4"
+                          className="mb-8 text-2xl text-title font-600"
+                        >
+                          Last payment amount and date
+                        </Typography>
+                        <p className="text-para_light">
+                          <span className="text-secondary">$230</span>, Feb 23, 2024
+                        </p>
+                      </div>
+                      <div className="shrink-0 w-[5rem] aspect-square flex items-center justify-center border rounded-lg border-borderColor">
+                        <LastPayment />
+                      </div>
+                    </div>
+                  </Link>
+                </Grid>
+                <Grid item lg={6} className="basis-full">
+                  <div className="flex items-center justify-between gap-10 p-24 rounded-lg bg-bgGrey ">
+                    <div>
+                      <Typography
+                        component="h4"
+                        className="mb-8 text-2xl text-title font-600"
+                      >
+                        Next payment amount and date
+                      </Typography>
+                      <p className="text-para_light">
+                        <span className="text-secondary">$230</span>, Feb 23, 2024
+                      </p>
+                    </div>
+                    <div className="shrink-0 w-[5rem] aspect-square flex items-center justify-center border rounded-lg border-borderColor">
+                      <LastPayment />
+                    </div>
+                  </div>
+                </Grid>
+              </Grid>
             </CustomTabPanel>
             <CustomTabPanel value={value} index={1}>
               Item Two
@@ -297,9 +347,9 @@ export default function ClientDetail() {
           </Box>
           <div className="h-24" />
         </div>
-      </div>
+      </div >
       <AddAgentModel isOpen={isOpenAddModal} setIsOpen={setIsOpenAddModal} />
-
+      <EditProfile isOpen={isOpenAddModal} setIsOpen={setIsOpenAddModal} />
     </>
   );
 }
