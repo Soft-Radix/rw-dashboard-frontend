255ad0681e6ffaf768d7d7e6050fd9627e5df357
Suhani <suhani@softradix.in>
2024-05-22 20:34:56 +0530

    Column CRUD done

--- src/app/configs/navigationConfig.tsx
+++ src/app/configs/navigationConfig.tsx
@@ -40,7 +40,7 @@ export const getProjectNavItems = () => {
     type: "item",
     icon: "material-twotone:compress",
     customIcon: <SubProjectIcon />,
-    // url: "projects",
+    url: `projects/${project.id}`,
     end: true,
     isProject: true,
   }));
