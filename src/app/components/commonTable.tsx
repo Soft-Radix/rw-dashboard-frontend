b2e07180a37b454cf261d23a339cbecd4b2699d9
lodhikomal <komal.rani@softradix.in>
2024-05-22 21:57:33 +0530

    update agent

--- src/app/components/commonTable.tsx
+++ src/app/components/commonTable.tsx
@@ -46,7 +46,7 @@ function CommonTable({
               : "bg-[#F7F9FB] text-sm border-b-2 border-solid border-[#EDF2F6]"
           } `}
         >
-          <TableRow {...headingRowProps}>
+          <TableRow {...headingRowProps} >
             {headings.map((item, index) => (
               <TableCell
                 className={`th ${index === 0 ? "pl-20" : ""} w-[${100 / headings?.length}%]`}
--- src/app/pages/agents/AgentsList.tsx
+++ src/app/pages/agents/AgentsList.tsx
@@ -215,6 +215,7 @@ export default function AgentsList() {
         setIsOpen={setIsOpenAddModal}
         fetchAgentList={fetchAgentList}
         isEditing={false}
+       
       />
     </>
   );
