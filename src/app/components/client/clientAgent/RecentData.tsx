c1afd9ac801bd1ecd2bf25b0a83c172bda1ef58a
Jatinder <vidushi@softradix.in>
2024-06-04 11:57:22 +0530

    agent issue

--- src/app/components/client/clientAgent/RecentData.tsx
+++ src/app/components/client/clientAgent/RecentData.tsx
@@ -12,69 +12,38 @@ const recentClients = [
     name: "Katherine",
     description: "Katherine has successfully completed the logo design task.",
   },
-  {
-    img: "female-02.jpg",
-    name: "Gabrielle",
-    description: "Gabrielle has successfully completed the logo design task.",
-  },
-  {
-    img: "female-03.jpg",
-    name: "Jennifer",
-    description: "Jennifer has successfully completed the logo design task.",
-  },
-  {
-    img: "female-01.jpg",
-    name: "Amanda",
-    description: "Amanda has successfully completed the logo design task.",
-  },
-  {
-    img: "male-02.jpg",
-    name: "Katherine",
-    description: "Katherine has successfully completed the logo design task.",
-  },
-  {
-    img: "female-02.jpg",
-    name: "Gabrielle",
-    description: "Gabrielle has successfully completed the logo design task.",
-  },
-  {
-    img: "female-03.jpg",
-    name: "Jennifer",
-    description: "Jennifer has successfully completed the logo design task.",
-  },
 ];
 
 function RecentData() {
   return (
-    <div className="shadow-md bg-white px-[18px] py-20 rounded-lg">
-      <Typography className="text-xl font-600 mb-10 bg-[#2C334C]">
-        2C334C
-      </Typography>
-      <div>
+    <div className="shadow-md bg-white rounded-lg overflow-hidden  h-full">
+      <div className="bg-[#2C334C] px-6 py-4">
+        <Typography className="text-xl font-600 text-white">
+          Recent Clients
+        </Typography>
+      </div>
+      <div className="px-[20px] py-4 space-y-4">
         {recentClients.map((item, index) => (
-          <div
-            className="flex gap-14 py-14 border-b border-b-[#EDF2F6]"
-            key={index}
-          >
-            <span className="shrink-0">
+          <div className="flex items-start gap-10 my-[20px]" key={index}>
+            {/* <span className="shrink-0">
               <img
                 className="rounded-full object-cover"
                 src={`/assets/images/avatars/${item.img}`}
                 height={34}
                 width={34}
-                alt=""
+                alt={item.name}
               />
-            </span>
-            <div className="grow">
-              <div className="flex items-center justify-between mb-6">
-                <Typography className="font-600">{item.name}</Typography>
-                <Typography className="text-grey-600 text-sm">
-                  Feb 12,2024
-                </Typography>
-              </div>
+            </span> */}
+            <div className="bg-[#F6F6F6] rounded-[10px] p-[16px] ">
               <Typography className="text-grey-600 text-sm">
                 {item.description}
               </Typography>
+              <div className="flex items-center justify-between mb-2">
+                {/* <Typography className="font-600">{item.name}</Typography> */}
+                <Typography className="text-grey-600 text-xs">
+                  Feb 12, 2024
+                </Typography>
+              </div>
             </div>
           </div>
         ))}
