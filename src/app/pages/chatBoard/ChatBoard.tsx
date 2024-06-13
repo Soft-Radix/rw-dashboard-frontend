71c3580c4796c1f81f8dee6d9a29ba9f75600459
Amit <amit@softradix.in>
2024-06-13 15:14:12 +0530

    updated

--- src/app/pages/chatBoard/ChatBoard.tsx
+++ src/app/pages/chatBoard/ChatBoard.tsx
@@ -112,7 +112,7 @@ function ChatBoard() {
           </div>
         ) : (
           <div className="w-full flex flex-col items-center justify-center gap-3">
-            <img src={"public/assets/images/pages/chat-board/no-msg.png"} />
+            <img src={import.meta.env.VITE_API_BASE_IMAGE_URL+"chat/no-msg.png"} />
             <h2 className="font-bold" style={{ color: "#111827" }}>
               No Message !
             </h2>
---
+++ src/app/pages/whiteBoard/i18n/en.ts
@@ -0,0 +1,5 @@
+const locale = {
+  TITLE: "White Board",
+};
+
+export default locale;
