46dae502ba6c6b1600f04d13e3e2feb7f3cc2bf9
komalrani24 <komal.rani@softradix.in>
2024-06-21 15:12:56 +0530

    pull changes

--- src/app/components/client/Subscription/ItemTable.tsx
+++ src/app/components/client/Subscription/ItemTable.tsx
@@ -1,10 +1,46 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import CommonTable from "../../commonTable";
 
-import { TableCell, TableRow, useTheme } from "@mui/material";
+import {
+  TableCell,
+  TableRow,
+  Tooltip,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import { useFormik } from "formik";
 import { getLabelByValue } from "src/utils";
 import moment from "moment";
+
+export const TruncateText = ({ text, maxWidth }) => {
+  const [isTruncated, setIsTruncated] = useState(false);
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    if (textRef.current) {
+      const textWidth = textRef.current.scrollWidth;
+      setIsTruncated(textWidth > maxWidth);
+    }
+  }, [text, maxWidth]);
+
+  return (
+    <Tooltip title={text} enterDelay={500} disableHoverListener={!isTruncated}>
+      <Typography
+        ref={textRef}
+        noWrap
+        style={{
+          maxWidth: `${maxWidth}px`,
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          display: "inline-block",
+          whiteSpace: "nowrap",
+        }}
+      >
+        {text}
+      </Typography>
+    </Tooltip>
+  );
+};
 
 const ItemTable = ({ rows }) => {
   const theme = useTheme();
@@ -52,81 +88,70 @@ const ItemTable = ({ rows }) => {
         }}
       >
         <>
-          {data?.map((row, index) => {
-            console.log(row,"rweyrwigfwfgeuf");
-            return (
-              <TableRow
-                key={index}
-                sx={{
-                  "&:last-child td, &:last-child th": { border: 0 },
-                  "& td": {
-                    borderBottom: "1px solid #EDF2F6",
-                    paddingTop: "12px",
-                    paddingBottom: "12px",
-                    color: theme.palette.primary.main,
-                  },
-                }}
+          {data?.map((row, index) => (
+            <TableRow
+              key={index}
+              sx={{
+                "&:last-child td, &:last-child th": { border: 0 },
+                "& td": {
+                  borderBottom: "1px solid #EDF2F6",
+                  paddingTop: "12px",
+                  paddingBottom: "12px",
+                  color: theme.palette.primary.main,
+                },
+              }}
+            >
+              <TableCell
+                scope="row"
+                className="items-center gap-8 font-500 flex-col sm:flex-row"
               >
-                <TableCell
-                  scope="row"
-                  className="items-center gap-8 font-500 flex-col sm:flex-row"
-                >
-                  {row?.product_name ? row?.product_name : "---"}
-                </TableCell>
-                <TableCell align="center" className="font-500">
-                  {row?.description ? row?.description : "---"}
-                </TableCell>
+                {row?.product_name ? row?.product_name : "---"}
+              </TableCell>
+              <TableCell align="center" className="font-500">
+                {row?.description ? (
+                  <TruncateText text={row?.description} maxWidth={200} />
+                ) : (
+                  "---"
+                )}
+              </TableCell>
 
-                <TableCell align="center" className="font-500">
-                  {getLabelByValue(row.billing_frequency)}
-                </TableCell>
+              <TableCell align="center" className="font-500">
+                {getLabelByValue(row.billing_frequency)}
+              </TableCell>
 
-                <TableCell align="center" className="font-500">
-                  {rows?.no_of_payments ? rows?.no_of_payments : ""}
-                </TableCell>
-                <TableCell align="center" className="font-500">
-                  {/* {row?.unit_discount ? row?.unit_discount : "N/A"} */}
-                  {row?.unit_discount
-                    ? `${row?.unit_discount_type == 2 ? "$" : ""}${
-                        row.unit_discount
-                      }${row?.unit_discount_type == 1 ? "%" : ""}`
-                    : "N/A"}
-                </TableCell>
-                <TableCell align="center" className="font-500">
-                  ${row?.unit_price ? row?.unit_price : "---"}
-                </TableCell>
-                <TableCell
-                  align="center"
-                  className="whitespace-nowrap font-500"
-                >
-                  ${row?.net_price ? row?.net_price : "---"}
-                </TableCell>
+              <TableCell align="center" className="font-500">
+                {rows?.no_of_payments ? rows?.no_of_payments : ""}
+              </TableCell>
+              <TableCell align="center" className="font-500">
+                {/* {row?.unit_discount ? row?.unit_discount : "N/A"} */}
+                {row?.unit_discount
+                  ? `${row?.unit_discount_type == 2 ? "$" : ""}${
+                      row.unit_discount
+                    }${row?.unit_discount_type == 1 ? "%" : ""}`
+                  : "N/A"}
+              </TableCell>
+              <TableCell align="center" className="font-500">
+                ${row?.unit_price ? row?.unit_price : "---"}
+              </TableCell>
+              <TableCell align="center" className="whitespace-nowrap font-500">
+                ${row?.net_price ? row?.net_price : "---"}
+              </TableCell>
 
-                <TableCell
-                  align="center"
-                  className="whitespace-nowrap font-500"
-                >
-                  {row?.quantity ? row?.quantity : "---"}
-                </TableCell>
-                <TableCell
-                  align="center"
-                  className="whitespace-nowrap font-500"
-                >
-                  {row?.billing_frequency == 1
-                    ? "Fixed Number"
-                    : "Automatically" || "Automatically"}
-                </TableCell>
-                <TableCell
-                  align="center"
-                  className="whitespace-nowrap font-500"
-                >
-                  {row?.createdAt
-                    ? moment(row?.createdAt).format("DD/MM/yyyy")
-                    : "N/A"}
-                </TableCell>
-              </TableRow>
-            );
-          })}
+              <TableCell align="center" className="whitespace-nowrap font-500">
+                {row?.quantity ? row?.quantity : "---"}
+              </TableCell>
+              <TableCell align="center" className="whitespace-nowrap font-500">
+                {rows?.billing_terms == 1
+                  ? "Fixed Number"
+                  : "Automatically" || "N/A"}
+              </TableCell>
+              <TableCell align="center" className="whitespace-nowrap font-500">
+                {rows?.subscription_start_date
+                  ? moment(rows?.subscription_start_date).format("DD/MM/yyyy")
+                  : "N/A"}
+              </TableCell>
+            </TableRow>
+          ))}
         </>
       </CommonTable>
       <div className="bg-[#F7F9FB] mt-[44px] mx-[16px] px-[22px] py-[18px] rounded-8">
